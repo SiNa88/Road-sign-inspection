d1e9b6acc10558878ec1a5f6c7f053e849241540
Narges Mehran <39765584+SiNa88@users.noreply.github.com>
2022-10-27 17:05:54 +0200

    Update run.sh

--- framing/run.sh
+++ framing/run.sh
@@ -4,13 +4,18 @@
 
 input_file=${1}
 input_directory=${2}
-output_directory=${3}
-mq_write=${4}
+#output_directory=${3}
+#mq_write=${4}
 
 echo '#'
 echo '#  Starting Process: '
 echo '#'
 
-python3 split.py "${input_file}" "${input_directory}"
+START=$(date +%s)
+python split.py "${input_file}" "${input_directory}"
 
+ls ${input_directory}
+END=$(date +%s)
+DIFF=$(( $END - $START ))
+echo "It took $DIFF seconds" && exit
 echo '  Done'
